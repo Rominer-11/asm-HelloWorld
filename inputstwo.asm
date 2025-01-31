ebefbdbaab0a1aae63009639bbe845e5f18eb143
rominer_11 <augustcho2000@gmail.com>
2025-01-30 22:02:25 -0800

    inputs two spits out strange things now.

--- inputstwo.asm
+++ inputstwo.asm
@@ -2,13 +2,13 @@
 ; say hello
 
 section .data
-	MOne: db "Give me a number: "
-	MOne_L: equ $ - MOne
-	MTwo: db "That number plus two is... "
-	MTwo_L: equ $ - MTwo
+	MOne db "Give me a one digit number: "
+	MOne_L equ $ - MOne
+	MTwo db "That number plus two is... "
+	MTwo_L equ $ - MTwo
 
 section .bss
-	number_V resb 255
+	number_V resb 1
 
 section .text
 	global _start
@@ -17,30 +17,22 @@ _start:
 	mov ebx, 1
 	mov ecx, MOne
 	mov edx, MOne_L
-	int 80h 
+	int 80h ; prints msg one 
 
 	mov eax, 3
 	mov ebx, 0
 	mov ecx, number_V ; variable to input into
 	mov edx, 255 ; size of the variable
-	int 80h ;
-
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, MTwo
-	mov edx, MTwo_L
-	int 80h
-
-	mov eax, number_V
-	mov ebx, 2
-	add eax, ebx
-	mov [number_V], eax
+	int 80h ; input variable
 
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, number_V
-	mov edx, 255
-	int 80h ; print sum
+	inc ecx
+	push ecx
+	mov ecx, esp
+	mov edx, 128
+	int 80h ; prints out num
 
 	mov eax, 1
 	mov ebx, 0
